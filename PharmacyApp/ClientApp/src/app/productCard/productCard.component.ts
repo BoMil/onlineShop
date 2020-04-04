a4513b8e7a6a834f29ca236634bfae2430d18dc9
Boki <bojanmilicevic91@gmail.com>
2020-04-04 23:16:36 +0200

    Added new icons to angular app and finished styling of product card component.

--- PharmacyApp/ClientApp/src/app/productCard/productCard.component.ts
+++ PharmacyApp/ClientApp/src/app/productCard/productCard.component.ts
@@ -8,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCardComponent implements OnInit {
 
-  constructor() { }
+    // TODO: For development only, replace it with real data
+    priceRaw = 1345.59;
+    price = {
+        mainPrice: null,
+        priceDecimal: null
+    };
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.formatPrice();
+    }
+
+    /**
+     * It will format price from the backend and split it into main and decimal part
+     */
+    formatPrice(): void {
+        const priceArray = this.priceRaw.toFixed(2).split('');
+        const dotIndex = priceArray.indexOf('.');
+
+        if (dotIndex) {
+          priceArray[dotIndex] = ',';
+          this.price.priceDecimal = priceArray.splice(dotIndex + 1, 2).join('');
+          this.price.mainPrice = priceArray.join('');
+        }
+    }
 
 }
