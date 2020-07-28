cc9276ab531ee4ea2c2d17390433d28023b588bd
Boki <bojanmilicevic91@gmail.com>
2020-07-28 19:31:48 +0200

    Fixed the react bidirectional renderer

--- PharmacyApp/ClientApp/src/app/angular/react-bidirectional-renderer/react-bidirectional-renderer.component.ts
+++ PharmacyApp/ClientApp/src/app/angular/react-bidirectional-renderer/react-bidirectional-renderer.component.ts
@@ -1,30 +1,17 @@
 import {Component, Injector, Input, OnInit, OnDestroy} from '@angular/core';
 import { ReactBidirectionalApplication } from 'src/app/react/react-bidirectional-application';
 import { ModalService } from '../_services/modal.service';
-import { Subject, BehaviorSubject } from 'rxjs';
-import { Modal } from '../_interfaces/modal';
 
 @Component({
   selector: 'app-react-owc-renderer',
   template: `<div class="react-container" id="react-owc-renderer">
   </div>`
 })
-export class ReactBidirectionalRendererComponent implements OnInit, OnDestroy {
+export class ReactBidirectionalRendererComponent implements OnInit {
 
     constructor(public injector: Injector, private modalService: ModalService) { }
 
-    modal$: BehaviorSubject<Modal>;
-
     ngOnInit() {
-        this.modal$ = this.modalService.getModalSubscription();
-
-        ReactBidirectionalApplication.initialize('react-owc-renderer', this.injector, this.modal$);
-    }
-
-    ngOnDestroy() {
-        if (this.modal$) {
-            this.modal$.unsubscribe();
-            console.log('Modal is unsubscribed!');
-        }
+        ReactBidirectionalApplication.initialize('react-owc-renderer', this.injector, this.modalService.getModalSubscription());
     }
 }
